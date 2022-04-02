6462250079f57abcf75a76579022721782ef28f7
Kai Lyons <kai@potabi.com>
2022-04-02 14:54:33 -0600

    Some final setup

--- setup.py
+++ setup.py
@@ -3,8 +3,9 @@ from setuptools import find_packages, setup
 setup(
     name='swekil',
     packages=find_packages(include=['swekil']),
-    version='0.1.0',
-    description='A simple webkit browser',
+    version='1.0.0',
+    description='A simple webkit library',
     author='Kai Lyons',
+    url = "https://github.com/loralighte/py38-swekil",
     license='MIT',
 )
