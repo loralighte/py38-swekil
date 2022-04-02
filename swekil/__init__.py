af2d580f6af81bbfd0189b1bd438f45963457423
Kai Lyons <kai@potabi.com>
2022-04-02 14:43:32 -0600

    Cleanup dependencies

--- swekil/__init__.py
+++ swekil/__init__.py
@@ -1,10 +1,8 @@
 import sys
-from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5 import QtCore
 from PyQt5.QtWebKit import *
 from PyQt5.QtWebKitWidgets import *
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
+from PyQt5.QtWidgets import QApplication
 
 app = QApplication(sys.argv)
 
